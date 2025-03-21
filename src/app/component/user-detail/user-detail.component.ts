b3f9342ad5eee3f80f031b88268774854b06b5b1
m.ivanova <margaritawork76@gmail.com>
2025-03-21 23:03:12 +0200

    map

--- src/app/component/user-detail/user-detail.component.ts
+++ src/app/component/user-detail/user-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../service/user.service';
-import { Response } from '../../../interface/response.interface';
 import { User } from '../../../interface/user.interface';
+import { Coordinates } from '../../../interface/coordinates.interface';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-user-detail',
@@ -11,10 +12,18 @@ import { User } from '../../../interface/user.interface';
   styleUrl: './user-detail.component.css'
 })
 export class UserDetailComponent implements OnInit {
-  // response: Response
   user: User;
   mode: 'edit' | 'locked' = 'locked';
   buttonText: 'Save Changes' | 'Edit' = 'Edit';
+
+  marker = new Leaflet.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconSize: [32, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  });
 
   constructor(private activateRoute: ActivatedRoute, private userService: UserService) {
 
@@ -22,15 +31,8 @@ export class UserDetailComponent implements OnInit {
   ngOnInit(): void {
     this.user = (<User>(this.activateRoute.snapshot.data['resolveResponse'].results[0]));
     console.log(this.user)
-    // this.activateRoute.paramMap.subscribe((params: ParamMap) => {
-    //   console.log(params.get('uuid')!);
-    //   this.userService.getUser(params.get('uuid')!).subscribe(
-    //     (response: any) => {
-    //       console.log(response)
-    //       this.response = response;
-    //     }
-    //   );
-    // });
+    this.loadMap(this.user.coordinate)
+
   }
 
   changeMode(mode: 'edit' | 'locked'): void {
@@ -41,5 +43,25 @@ export class UserDetailComponent implements OnInit {
     if (mode === 'edit') {
       console.log('Update user.')
     }
+  }
+
+  private loadMap(coordinate: Coordinates): void {
+    const map = Leaflet.map('map', {
+      center: [coordinate.latitude, coordinate.longitude],
+      zoom: 8
+    });
+
+    const mainLayer = Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      tileSize: 512,
+      zoomOffset: -1,
+      minZoom: 1,
+      maxZoom: 30
+      , crossOrigin: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+
+    mainLayer.addTo(map);
+    const marker = Leaflet.marker([coordinate.latitude, coordinate.longitude], { icon: this.marker });
+    marker.addTo(map).bindPopup(`${this.user.firstName}'s Location`).openPopup();
   }
 }
